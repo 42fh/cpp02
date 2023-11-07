ab92e0821bc46b577bc7289c948d21b390dc762e
fheld <fheld@student.42berlin.de>
2023-11-07 22:06:34 +0100

    added msgs in constructors

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -2,11 +2,11 @@
 
 const int Fixed::fractionalbits = 8;
 
-Fixed::Fixed(): num(0){}
-Fixed::~Fixed(){}
-Fixed::Fixed(const Fixed& a): num(a.num) {}
-const Fixed &Fixed::operator=(const Fixed& other){num = other.num; return *this;}
+Fixed::Fixed(): num(0){std::cout << "Default constructor called" << std::endl;}
+Fixed::~Fixed(){std::cout << "Destructor called" << std::endl;}
+Fixed::Fixed(const Fixed& a): num(a.num) {std::cout << "Copy constructor called" << std::endl;}
+const Fixed &Fixed::operator=(const Fixed& other){std::cout << "Copy assignment operator called" << std::endl;num = other.num; return *this;}
 
-int Fixed::getRawBits(void) const{return num;}
+int Fixed::getRawBits(void) const{std::cout << "getRawBits member function called" << std::endl;return num;}
 
-void Fixed::setRawBits(int const a){num = a;}
+void Fixed::setRawBits(int const a){std::cout << "" << std::endl;num = a;}

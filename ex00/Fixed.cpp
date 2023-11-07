0632f97f093f4420b169fa1672e1785022cccada
fheld <fheld@student.42berlin.de>
2023-11-07 15:26:52 +0100

    reducing the version to what is actually needed in ex00

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -5,20 +5,8 @@ const int Fixed::fractionalbits = 8;
 Fixed::Fixed(): num(0){}
 Fixed::~Fixed(){}
 Fixed::Fixed(const Fixed& a): num(a.num) {}
-Fixed::Fixed(int a){num = POW2_8 * a;}
-Fixed::Fixed(float a){num = (float)POW2_8 * a;}
-
 const Fixed &Fixed::operator=(const Fixed& other){num = other.num; return *this;}
 
 int Fixed::getRawBits(void) const{return num;}
 
 void Fixed::setRawBits(int const a){num = a;}
-
-int Fixed::toInt(void) const{return roundf(this->toFloat());}
-
-float Fixed::toFloat(void) const{ return (float)num / (float)POW2_8;}
-
-std::ostream& operator<<(std::ostream& os, const Fixed& obj) {
-	os << obj.toFloat();
-	return os;
-}
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -14,19 +14,12 @@ private:
 public:
 	// 🐟⁉️
 	Fixed();
-	Fixed(const int);
-	Fixed(const float);
-	// Fixed(Fixed&);
 	Fixed(const Fixed&);
 	const Fixed& operator=(const Fixed&);
 	~Fixed();
 
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
-	int toInt(void)const;
-	float toFloat(void) const;
 };
-
-std::ostream& operator<<(std::ostream& os, const Fixed& obj);
 
 #endif

0cf934df73f3a0965b54acadcbe3e0294721a854
Replit user <>
2023-11-07 13:34:37 +0000

    changes to the copy assignemtn op

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -2,6 +2,9 @@
 #define FIXED_HPP
 
 #include <iostream>
+#include <cmath>
+
+#define POW2_8 256
 
 class Fixed
 {
@@ -11,8 +14,8 @@ private:
 public:
 	// 🐟⁉️
 	Fixed();
-	Fixed(int);
-	Fixed(float);
+	Fixed(const int);
+	Fixed(const float);
 	// Fixed(Fixed&);
 	Fixed(const Fixed&);
 	const Fixed& operator=(const Fixed&);
@@ -20,6 +23,8 @@ public:
 
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
+	int toInt(void)const;
+	float toFloat(void) const;
 };
 
 std::ostream& operator<<(std::ostream& os, const Fixed& obj);

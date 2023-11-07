fe412ce02ac2237bcf38db1c4551a73c691a706d
fheld <fheld@student.42berlin.de>
2023-11-07 16:03:49 +0100

    added operator overloads

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,50 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+#define POW2_8 256
+
+class Fixed
+{
+private:
+	int num;
+	static const int fractionalbits;
+public:
+	// 🐟⁉️
+	Fixed();
+	Fixed(const int);
+	Fixed(const float);
+	// Fixed(Fixed&);
+	Fixed(const Fixed&);
+	const Fixed& operator=(const Fixed&);
+	~Fixed();
+
+	bool operator==(const Fixed&);
+	bool operator!=(const Fixed&);
+	bool operator<(const Fixed&);
+	bool operator<=(const Fixed&);
+	bool operator>(const Fixed&);
+	bool operator>=(const Fixed&);
+
+	Fixed operator+(const Fixed&);
+	Fixed operator-(const Fixed&);
+	Fixed operator*(const Fixed&);
+	Fixed operator/(const Fixed&);
+
+	Fixed& operator++(void);
+	Fixed operator++(int);
+	Fixed& operator--(void);
+	Fixed operator--(int);
+
+
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	int toInt(void)const;
+	float toFloat(void) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& obj);
+
+#endif
